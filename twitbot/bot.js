b88efb8183618a366b495e9c8ac612de383a3cb2
lj020326 <lj020326@users.noreply.github.com>
2015-02-20 12:35:36 -0500

    working version

--- twitbot/bot.js
+++ twitbot/bot.js
@@ -3,115 +3,134 @@
 //  class for performing various twitter actions
 //
 var Twit = require('../lib/twitter');
-var nconf = require('nconf');
 
-var path = require('path');
-var log4js = require('log4js');
-var modulename = path.basename(process.argv[1]);
-log4js.configure('log4js_config.json');
-var logger = log4js.getLogger(modulename);
-
-var Bot = module.exports = function(conf) { 
-        
-    var twit_auth = conf.get('twitterAuth');
-    logger.debug("Bot.init(): twit_auth="+twit_auth);
-    
-    //    this.twit = new Twit(config);
-    this.twit = new Twit(twit_auth);
+var Bot = module.exports = function(config) { 
+  this.twit = new Twit(config);
 };
 
 //
 //  post a tweet
 //
 Bot.prototype.tweet = function (status, callback) {
-    if(typeof status !== 'string') {
-	return callback(new Error('tweet must be of type String'));
-    } else if(status.length > 140) {
-	return callback(new Error('tweet is too long: ' + status.length));
-    }
-    this.twit.post('statuses/update', { status: status }, callback);
+  if(typeof status !== 'string') {
+    return callback(new Error('tweet must be of type String'));
+  } else if(status.length > 140) {
+    return callback(new Error('tweet is too long: ' + status.length));
+  }
+  this.twit.post('statuses/update', { status: status }, callback);
 };
 
 //
 //  choose a random friend of one of your followers, and follow that user
 //
 Bot.prototype.mingle = function (callback) {
-    var self = this;
-    
-    this.twit.get('followers/ids', function(err, reply) {
-	if(err) { return callback(err); }
-	
-	var followers = reply.ids
+  var self = this;
+  
+  this.twit.get('followers/ids', function(err, reply) {
+      if(err) { return callback(err); }
+      
+      var followers = reply.ids
         , randFollower  = randIndex(followers);
         
-	self.twit.get('friends/ids', { user_id: randFollower }, function(err, reply) {
-            if(err) { return callback(err); }
-            
-            var friends = reply.ids
+      self.twit.get('friends/ids', { user_id: randFollower }, function(err, reply) {
+          if(err) { return callback(err); }
+          
+          var friends = reply.ids
             , target  = randIndex(friends);
             
-            self.twit.post('friendships/create', { id: target }, callback); 
+          self.twit.post('friendships/create', { id: target }, callback); 
         })
     })
-};
-
-//
-// added new searchFollow function per apcoder.com
-//
-Bot.prototype.searchFollow = function (params, callback) {
-    var self = this;
-
-    self.twit.get('search/tweets', params, function (err, reply) {
-	if(err) return callback(err);
-	
-	var tweets = reply.statuses;
-	if (tweets.length==0) {
-	    return callback(new Error('cannot find tweets for: ' + params.q));
-	}
-	
-	var target = randIndex(tweets).user.id_str;
-	self.twit.post('friendships/create', { id: target }, callback);
-    });
-};
-
-//
-// retweet
-//
-Bot.prototype.retweet = function (params, callback) {
-    var self = this;
-    
-    self.twit.get('search/tweets', params, function (err, reply) {
-	if(err) return callback(err);
-	
-	var tweets = reply.statuses;
-	
-	if (tweets.length==0) {
-	    return callback(new Error('cannot find tweets for: ' + params.q));
-	}
-	var randomTweet = randIndex(tweets);
-	self.twit.post('statuses/retweet/:id', { id: randomTweet.id_str }, callback);
-	
-    });
 };
 
 //
 // favorite a tweet
 //
 Bot.prototype.favorite = function (params, callback) {
-    var self = this;
+	var self = this;
 
-    self.twit.get('search/tweets', params, function (err, reply) {
-	if(err) return callback(err);
+	self.twit.get('search/tweets', params, function (err, reply) {
+		if(err) return callback(err);
 
-	var tweets = reply.statuses;
+		var tweets = reply.statuses;
 
-	if (tweets.length==0) {
-	    return callback(new Error('cannot find tweets for:'+params.q));
-	};
+		if (tweets.length==0) {
+			return callback(new Error('cannot find tweets for:'+params.q));
+		}
 
-	var randomTweet = randIndex(tweets);
-	self.twit.post('favorites/create', { id: randomTweet.id_str }, callback);
-    });
+		var randomTweet = randIndex(tweets);
+		self.twit.post('favorites/create', { id: randomTweet.id_str }, callback);
+	});
+};
+
+//
+// added new searchFollow function per apcoder.com
+//
+Bot.prototype.searchFollow = function (params, callback) {
+	var self = this;
+
+	self.twit.get('search/tweets', params, function (err, reply) {
+		if(err) return callback(err);
+
+		var tweets = reply.statuses;
+		if (tweets.length==0) {
+			return callback(new Error('cannot find tweets for: ' + params.q));
+		}
+
+		var target = randIndex(tweets).user.id_str;
+		self.twit.post('friendships/create', { id: target }, callback);
+	});
+};
+
+
+//
+// retweet
+//
+Bot.prototype.retweet = function (params, callback) {
+	var self = this;
+
+	self.twit.get('search/tweets', params, function (err, reply) {
+		if(err) return callback(err);
+
+		var tweets = reply.statuses;
+
+		if (tweets.length==0) {
+			return callback(new Error('cannot find tweets for: ' + params.q));
+		}
+		var randomTweet = randIndex(tweets);
+		self.twit.post('statuses/retweet/:id', { id: randomTweet.id_str }, callback);
+	});
+};
+
+
+//
+//  thank users
+//
+
+Bot.prototype.dm_thanks = function (callback) {
+	var self = this;
+
+	this.twit.get('followers/ids', function(err, reply) {
+		if(err) return callback(err);
+
+		var followers = reply.ids;
+
+		if (followers.length==0) {
+			return callback(new Error('cannot find followers for pruning'));
+		}
+
+		for (var i = 0; i < followers.length; i++) {
+			var target = followers[i];
+			if (!~already_sent_reply.indexOf(target)) {
+				var msg = "Thanks for following!";
+				self.twit.post('direct_messages/new', { user_id: target, text: msg }, callback);
+//		logger.debug("Sent direct message to " + target);
+				console.log("Sent direct message to " + target);
+			}
+		}
+
+	});
+
 };
 
 
@@ -119,79 +138,39 @@ Bot.prototype.favorite = function (params, callback) {
 //  prune your followers list; unfollow a friend that hasn't followed you back
 //
 Bot.prototype.prune = function (callback) {
-    var self = this;
-    
-    this.twit.get('followers/ids', function(err, reply) {
-	if(err) return callback(err);
-	
-	var followers = reply.ids;
-
-	if (followers.length==0) {
-	    return callback(new Error('cannot find followers for pruning'));
-	}
-
-	self.twit.get('friends/list', function(err, reply) {
-            if(err) return callback(err);
-            
-            var friends = reply.users
+  var self = this;
+  
+  this.twit.get('followers/ids', function(err, reply) {
+      if(err) return callback(err);
+      
+      var followers = reply.ids;
+      
+      self.twit.get('friends/ids', function(err, reply) {
+          if(err) return callback(err);
+          
+          var friends = reply.ids
             , pruned = false;
+          
+          while(!pruned) {
+            var target = randIndex(friends);
             
-            while(!pruned) {
-		var target = randIndex(friends);
-		
-		if(!~followers.indexOf(target.id) && target.followers_count<10) {
-		    pruned = true;
-		    self.twit.post('friendships/destroy', { id: target }, callback);
-		}
+            if(!~followers.indexOf(target)) {
+              pruned = true;
+              self.twit.post('friendships/destroy', { id: target }, callback);   
             }
-	});
-    });
-};
-    
-// def direct_messages():
-//     new_followers = API.followers(user)
-
-//     for i in new_followers:
-//         newDM = raw_input (i.from_user + "send follower DM?" + "Y/N" )
-//         if newDM.lower() == "n":
-//             print i.from_user + " was not messaged"
-//             print "Now returning to the Main Menu."
-//     else:
-//         api.send_direct_message(user_id = i.from_user, text = "message text here"
-//         print "You messaged " + i.from_user
-
-
-//
-//  prune your followers list; unfollow a friend that hasn't followed you back
-//
-
-Bot.prototype.dm_thanks = function (callback) {
-    var self = this;
-    
-    this.twit.get('followers/ids', function(err, reply) {
-	if(err) return callback(err);
-	
-	var followers = reply.ids;
-	
-	if (followers.length==0) {
-	    return callback(new Error('cannot find followers for pruning'));
-	}
-	
-	for (var i = 0; i < followers.length; i++) {
-	    var target = friends[i];
-	    if (!~already_sent_reply.indexOf(target)) {
-		var msg = "Thanks for following!";
-		self.twit.post('direct_messages/new', { user_id: target, text: msg }, callback);
-		logger.debug("Sent direct message to " + target);
-	    }
-        }
-	
-    });
-    
-    
+          }
+      });
+  });
 };
 
 function randIndex (arr) {
-    var index = Math.floor(arr.length*Math.random());
-    return arr[index];
+  var index = Math.floor(arr.length*Math.random());
+  return arr[index];
+};
+
+//
+// choose a randox element from array
+//
+Bot.prototype.randIndex = function (arr) {
+	return randIndex(arr);
 };
---
+++ twitbot/twitbot.js
@@ -0,0 +1,135 @@
+//
+//  Twitbot - Twitter bot that mingles, follows, favorites, retweets, and prunes
+//  Also hopefully makes new friends along the way :)
+//
+
+//require('look').start();
+var config_app = require('../config_app');
+var config_user = require('../config_user-lee');
+var Bot = require('./bot');
+
+var logger = config_app.logger;
+var bot = new Bot(config_user.twitter_auth);
+
+logger.info('Starting');
+
+var probability_map = config_app.probability_map;
+logger.debug("probability_map="+probability_map);
+
+var qstring_arr = config_user.qstring_arr;
+logger.debug("qstring_arr="+qstring_arr);
+
+//get date string for today's date (e.g. '2011-01-01')
+function datestring () {
+    var d = new Date(Date.now() - 5*60*60*1000);  //est timezone
+    return d.getUTCFullYear()   + '-'
+	+  (d.getUTCMonth() + 1) + '-'
+	+   d.getDate();
+}
+
+function handleError(err) {
+    logger.error("response status:"+err.statusCode+", data="+err.data);
+}
+
+function doTwitterAction() {
+
+    logger.info("doTwitterAction: Starting");
+
+    bot.twit.get('followers/ids', function(err, reply) {
+		if(err) return handleError(err);
+		logger.info('# followers:' + reply.ids.length.toString());
+    });
+    
+    var rand = Math.random();
+	var twit_function="";
+
+	if (config_app.test_function != null) {
+		twit_function = config_app.test_function;
+	} else if(rand <= probability_map.mingle) { //  make a friend
+		twit_function = "mingle";
+	} else if(rand <= probability_map.favorite) { //  make a friend
+		twit_function = "favorite";
+	} else if(rand <= probability_map.follow) { //  make a friend
+		twit_function = "follow";
+	} else if(rand <= probability_map.retweet) { //  make a friend
+		twit_function = "retweet";
+	} else if(rand <= probability_map.prune) { //  make a friend
+		twit_function = "prune";
+	}
+
+	if(twit_function=="mingle") { //  make a friend
+		logger.info('bot.mingle: starting');
+		bot.mingle(function(err, reply) {
+			if(err) return handleError(err);
+
+			var name = reply.screen_name;
+			logger.debug("Mingle: followed @" + name);
+		});
+	} else if(twit_function=="favorite") {  // favorite a tweet
+		var qstring = bot.randIndex(qstring_arr);
+
+		var params = {
+				q: qstring
+			, since: datestring()
+			, result_type: "mixed"
+		};
+
+		logger.info('bot.favorite: qstring='+qstring);
+		bot.favorite(params, function(err, reply) {
+			if(err) return handleError(err);
+			logger.debug("Favorite: favorited response: " + reply.id);
+		});
+	} else if(twit_function=="follow") {  // do a targeted follow
+		var qstring = bot.randIndex(qstring_arr);
+		var params = {
+				q: qstring
+			, since: datestring()
+			, result_type: "mixed"
+		};
+
+		logger.info('bot.searchFollow: qstring='+qstring);
+		bot.searchFollow(params, function(err, reply) {
+			if(err) return handleError(err);
+
+			var name = reply.screen_name;
+			logger.debug("SearchFollow: followed @" + name);
+		});
+    } else if(twit_function=="retweet") {  // retweet
+		var qstring = bot.randIndex(qstring_arr);
+
+		var params = {
+				q: qstring
+			, since: datestring()
+			, result_type: "mixed"
+		};
+
+		logger.info('bot.retweet: qstring='+qstring);
+		bot.retweet(params, function(err, reply) {
+			if(err) return handleError(err);
+			logger.debug("Retweet: retweeted response: " + reply.id);
+		});
+	} else if(twit_function=="prune") { //  prune a friend
+		logger.info('bot.prune');
+		bot.prune(function(err, reply) {
+			if(err) return handleError(err);
+
+			var name = reply.screen_name;
+			logger.info('Prune: unfollowed @'+ name);
+		});
+	} else { // do nothing
+		logger.info('do nothing');
+		noop();
+	}
+
+	logger.debug("doTwitterAction: setting next runtime");
+    var constantTime = 1000*60*12;// 12 minutes
+    var randomTime = Math.floor(6*60*1000*Math.random());// upto 10 seconds
+    var nextRuntime = constantTime + randomTime;
+	setTimeout(function() { doTwitterAction() }, nextRuntime);// upto 10 seconds
+}
+
+
+logger.info("calling doTwitterAction()");
+
+doTwitterAction();
+
